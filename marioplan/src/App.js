3e2b1b92dd5813257b3e8cb3da01b66d4c9ccf62
Shaun <shaunjp@shaun-pelling.com>
2018-07-24 10:35:35 +0100

    lesson-7

--- marioplan/src/App.js
+++ marioplan/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import Navbar from './components/layout/Navbar'
 import Dashboard from './components/dashboard/Dashboard'
+import ProjectDetails from './components/projects/ProjectDetails'
 
 class App extends Component {
   render() {
@@ -10,7 +11,8 @@ class App extends Component {
         <div className="App">
           <Navbar />
           <Switch>
-            <Route path='/'component={Dashboard} />
+            <Route exact path='/'component={Dashboard} />
+            <Route path='/project/:id' component={ProjectDetails} />
           </Switch>
         </div>
       </BrowserRouter>
