8d96c871d7d8f1f0a369ebea54bbdc93d07cb875
Shaun <shaunjp@shaun-pelling.com>
2018-07-24 12:06:48 +0100

    lesson-11

--- marioplan/src/index.js
+++ marioplan/src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { createStore } from 'redux'
+import rootReducer from './store/reducers/rootReducer'
+import { Provider } from 'react-redux'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(rootReducer);
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
---
+++ marioplan/src/store/reducers/projectReducer.js
@@ -0,0 +1,7 @@
+const initState = {}
+
+const projectReducer = (state = initState, action) => {
+  return state;
+};
+
+export default projectReducer;
