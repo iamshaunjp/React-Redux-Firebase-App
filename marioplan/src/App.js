5584cb3ed71531171f81200b397dcad9c0b593dc
Shaun <shaunjp@shaun-pelling.com>
2018-07-24 11:01:29 +0100

    lesson-9

--- marioplan/src/App.js
+++ marioplan/src/App.js
@@ -5,6 +5,7 @@ import Dashboard from './components/dashboard/Dashboard'
 import ProjectDetails from './components/projects/ProjectDetails'
 import SignIn from './components/auth/SignIn'
 import SignUp from './components/auth/SignUp'
+import CreateProject from './components/projects/CreateProject'
 
 class App extends Component {
   render() {
@@ -17,6 +18,7 @@ class App extends Component {
             <Route path='/project/:id' component={ProjectDetails} />
             <Route path='/signin' component={SignIn} />
             <Route path='/signup' component={SignUp} />
+            <Route path='/create' component={CreateProject} />
           </Switch>
         </div>
       </BrowserRouter>
