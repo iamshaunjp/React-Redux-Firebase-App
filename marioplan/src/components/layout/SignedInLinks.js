7dd7283fd424c4c8c7d55e3ae2e0ff3c7fe1f739
Shaun <shaunjp@shaun-pelling.com>
2018-07-24 11:55:57 +0100

    lesson-10

--- marioplan/src/components/layout/SignedInLinks.js
+++ marioplan/src/components/layout/SignedInLinks.js
@@ -5,7 +5,7 @@ const SignedInLinks = () => {
   return (
     <div>
       <ul className="right">
-        <li><NavLink to='/'>New Project</NavLink></li>
+        <li><NavLink to='/create'>New Project</NavLink></li>
         <li><NavLink to='/'>Log Out</NavLink></li>
         <li><NavLink to='/' className="btn btn-floating pink lighten-1">NN</NavLink></li>
       </ul>
--- marioplan/src/components/layout/SignedOutLinks.js
+++ marioplan/src/components/layout/SignedOutLinks.js
@@ -5,8 +5,8 @@ const SignedOutLinks = () => {
   return (
     <div>
       <ul className="right">
-        <li><NavLink to='/'>Signup</NavLink></li>
-        <li><NavLink to='/'>Login</NavLink></li>
+        <li><NavLink to='/signup'>Signup</NavLink></li>
+        <li><NavLink to='/signin'>Login</NavLink></li>
       </ul>
     </div>
   )
