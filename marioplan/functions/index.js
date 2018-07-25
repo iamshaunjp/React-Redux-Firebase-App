61f3c5d26eeb2f70b5470e686f5ed2ec3be2783f
Shaun <shaunjp@shaun-pelling.com>
2018-07-25 21:56:54 +0100

    lesson-37

--- marioplan/functions/index.js
+++ marioplan/functions/index.js
@@ -23,3 +23,21 @@ exports.projectCreated = functions.firestore
     return createNotification(notification);
 
 });
+
+exports.userJoined = functions.auth.user()
+  .onCreate(user => {
+    
+    return admin.firestore().collection('users')
+      .doc(user.uid).get().then(doc => {
+
+        const newUser = doc.data();
+        const notification = {
+          content: 'Joined the party',
+          user: `${newUser.firstName} ${newUser.lastName}`,
+          time: admin.firestore.FieldValue.serverTimestamp()
+        };
+
+        return createNotification(notification);
+
+      });
+});
