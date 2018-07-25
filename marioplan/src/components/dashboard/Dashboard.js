7d9c51bd980d709368bb594ab00152d3615bfd15
Shaun <shaunjp@shaun-pelling.com>
2018-07-25 10:09:51 +0100

    lesson-26

--- marioplan/src/components/dashboard/Dashboard.js
+++ marioplan/src/components/dashboard/Dashboard.js
@@ -5,12 +5,12 @@ import { connect } from 'react-redux'
 import { firestoreConnect } from 'react-redux-firebase'
 import { compose } from 'redux'
 
+
+
 class Dashboard extends Component {
   render() {
-    
-    // console.log(this.props);
     const { projects } = this.props;
-    
+
     return (
       <div className="dashboard container">
         <div className="row">
--- marioplan/src/index.js
+++ marioplan/src/index.js
@@ -14,10 +14,13 @@ import fbConfig from './config/fbConfig'
 const store = createStore(rootReducer,
   compose(
     applyMiddleware(thunk.withExtraArgument({getFirebase, getFirestore})),
-    reactReduxFirebase(fbConfig), // redux binding for firebase
+    reactReduxFirebase(fbConfig, {attachAuthIsReady: true}), // redux binding for firebase
     reduxFirestore(fbConfig) // redux bindings for firestore
   )
 );
 
-ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
-registerServiceWorker();
+store.firebaseAuthIsReady.then(() => {
+  ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+  registerServiceWorker();
+});
+
