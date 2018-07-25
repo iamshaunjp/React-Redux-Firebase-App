8affaee5f262ada457e1c372f39c3e1d7c9bddd4
Shaun <shaunjp@shaun-pelling.com>
2018-07-25 14:50:54 +0100

    lesson-30

--- marioplan/src/components/projects/ProjectSummary.js
+++ marioplan/src/components/projects/ProjectSummary.js
@@ -5,7 +5,7 @@ const ProjectSummary = ({project}) => {
     <div className="card z-depth-0 project-summary">
       <div className="card-content grey-text text-darken-3">
         <span className="card-title ">{project.title}</span>
-        <p>Posted by The Net Ninja</p>
+        <p>Posted by {project.authorFirstName} {project.authorLastName}</p>
         <p className="grey-text">3rd September, 2am</p>
       </div>
     </div>
--- marioplan/src/store/actions/projectActions.js
+++ marioplan/src/store/actions/projectActions.js
@@ -1,12 +1,13 @@
 export const createProject = (project) => {
   return (dispatch, getState, {getFirestore}) => {
-    // make async call to database
     const firestore = getFirestore();
+    const profile = getState().firebase.profile;
+    const authorId = getState().firebase.auth.uid;
     firestore.collection('projects').add({
       ...project,
-      authorFirstName: 'Net',
-      authorLastName: 'Ninja',
-      authorId: 12345,
+      authorFirstName: profile.firstName,
+      authorLastName: profile.lastName,
+      authorId: authorId,
       createdAt: new Date()
     }).then(() => {
       dispatch({ type: 'CREATE_PROJECT_SUCCESS' });
