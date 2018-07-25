f72ca89f78b3d577b5fe8e574d4debbf52a3f434
Shaun <shaunjp@shaun-pelling.com>
2018-07-25 22:06:57 +0100

    lesson-39

--- marioplan/src/components/dashboard/Dashboard.js
+++ marioplan/src/components/dashboard/Dashboard.js
@@ -40,7 +40,7 @@ const mapStateToProps = (state) => {
 export default compose(
   connect(mapStateToProps),
   firestoreConnect([
-    { collection: 'projects' },
-    { collection: 'notifications', limit: 3}
+    { collection: 'projects', orderBy: ['createdAt', 'desc']},
+    { collection: 'notifications', limit: 3, orderBy: ['time', 'desc']}
   ])
 )(Dashboard)
