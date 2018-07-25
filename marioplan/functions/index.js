0421f8e79ed7129eaae76456f3d2b7a1f5d772b0
Shaun <shaunjp@shaun-pelling.com>
2018-07-25 21:53:00 +0100

    lesson-36

--- marioplan/functions/index.js
+++ marioplan/functions/index.js
@@ -1,8 +1,25 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
 
-// Create and Deploy Your First Cloud Functions
-// https://firebase.google.com/docs/functions/write-firebase-functions
+const createNotification = ((notification) => {
+  return admin.firestore().collection('notifications')
+    .add(notification)
+    .then(doc => console.log('notification added', doc));
+});
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello, ninjas!");
+
+exports.projectCreated = functions.firestore
+  .document('projects/{projectId}')
+  .onCreate(doc => {
+
+    const project = doc.data();
+    const notification = {
+      content: 'Added a new project',
+      user: `${project.authorFirstName} ${project.authorLastName}`,
+      time: admin.firestore.FieldValue.serverTimestamp()
+    }
+
+    return createNotification(notification);
+
 });
