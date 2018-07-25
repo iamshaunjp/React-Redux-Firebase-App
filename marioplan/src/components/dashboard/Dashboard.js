08bc81d3220fed1db01ef6e55e208409fa24edcf
Shaun <shaunjp@shaun-pelling.com>
2018-07-25 22:03:21 +0100

    lesson-38

--- marioplan/src/components/dashboard/Dashboard.js
+++ marioplan/src/components/dashboard/Dashboard.js
@@ -10,7 +10,7 @@ import { Redirect } from 'react-router-dom'
 
 class Dashboard extends Component {
   render() {
-    const { projects, auth } = this.props;
+    const { projects, auth, notifications } = this.props;
     if (!auth.uid) return <Redirect to='/signin' /> 
 
     return (
@@ -20,7 +20,7 @@ class Dashboard extends Component {
             <ProjectList projects={projects} />
           </div>
           <div className="col s12 m5 offset-m1">
-            <Notifications />
+            <Notifications notifications={notifications} />
           </div>
         </div>
       </div>
@@ -32,13 +32,15 @@ const mapStateToProps = (state) => {
   // console.log(state);
   return {
     projects: state.firestore.ordered.projects,
-    auth: state.firebase.auth
+    auth: state.firebase.auth,
+    notifications: state.firestore.ordered.notifications
   }
 }
 
 export default compose(
   connect(mapStateToProps),
   firestoreConnect([
-    { collection: 'projects' }
+    { collection: 'projects' },
+    { collection: 'notifications', limit: 3}
   ])
 )(Dashboard)
