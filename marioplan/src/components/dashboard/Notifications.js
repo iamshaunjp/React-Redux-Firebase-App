e4457e373a68ca6c0d215a9f567ded4f46b47d69
Shaun <shaunjp@shaun-pelling.com>
2018-07-25 19:06:53 +0100

    lesson-35

--- marioplan/src/components/dashboard/Notifications.js
+++ marioplan/src/components/dashboard/Notifications.js
@@ -1,9 +1,16 @@
 import React from 'react'
 
-const Notifications = () => {
+const Notifications = (props) => {
   return (
-    <div>
-      <p>Notifications</p>
+    <div className="section">
+      <div className="card z-depth-0">
+        <div className="card-content">
+          <span className="card-title">Notifications</span>
+          <ul className="online-users">
+            <li>Notification</li>
+          </ul>
+        </div>
+      </div>
     </div>
   )
 }
