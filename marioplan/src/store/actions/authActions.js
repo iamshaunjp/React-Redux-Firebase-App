df6ed648ad82cefaa9268a9f87a8147ddb54b6d6
seiya0324 <p@seiyamurayama.com>
2019-12-11 00:57:40 -0800

    update

---
+++ marioplan/src/store/actions/authActions.js
@@ -0,0 +1,15 @@
+export const signIn = credentials => {
+  return (dispatch, getState, { getFirebase }) => {
+    const firebase = getFirebase();
+
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(credentials.email, credentials.password)
+      .then(() => {
+        dispatch({ type: "LOGIN_SUCCESS" });
+      })
+      .catch(err => {
+        dispatch({ type: "LOGIN_ERROR", err });
+      });
+  };
+};
