9903aad51fd5aaf68fd3515f46d03daa1ae76e4e
alekseimets <41649499+alekseimets@users.noreply.github.com>
2020-09-15 23:59:02 +0300

    Update index.js
    I found one mistake.

--- marioplan/functions/index.js
+++ marioplan/functions/index.js
@@ -2,7 +2,7 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
 
-const createNotification = ((notification) => {
+const createNotification = (notification) => {
   return admin.firestore().collection('notifications')
     .add(notification)
     .then(doc => console.log('notification added', doc));
